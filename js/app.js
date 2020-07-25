7196c1dbbdb74849c1e8827ccb1f13c16685e463
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2020-07-26 01:17:36 +0200

    adding new tasks and shwoing if a task is done

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@
     },
     {
       content: "zjeść kolację",
-      done: false,
+      done: true,
     },
   ];
 
@@ -15,17 +15,41 @@
 
     for (const task of tasks) {
       htmlString += `
-         <li>
+         <li${task.done ? ' style="text-decoration: line-through"' : ""}>
             ${task.content}
-         </li>
+         </li$>
         `;
     }
 
     document.querySelector(".js-tasks").innerHTML = htmlString;
   };
 
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+
+    render();
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+    if (newTaskContent === "") {
+      return;
+    }
+
+    addNewTask(newTaskContent);
+  };
+
   const init = () => {
     render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
   };
 
   init();
